9f6549905ebbbfcc31da8a41c36bcb2dc1c8ff66
Elliot Su <sodinfeliz@gmail.com>
2024-02-19 17:06:45 +0800

    [REFACTOR] Refactor `general_algo.py`

--- src/general_algo.py
+++ src/general_algo.py
@@ -14,30 +14,29 @@ class GeneralAlgo:
     def prompt(cls, message: str, configs: dict):
         method = select_prompt(message, choices=list(cls._AVAILABLE_ALGO.keys()))
 
-        finished = False
-        all_configs = cls._AVAILABLE_ALGO[method]().get_params()
-        all_configs.update(configs[method])
+        algo_instance = cls._AVAILABLE_ALGO[method]()
+        algo_configs = algo_instance.get_params()
+        algo_configs.update(configs.get(method, {}))
 
         if confirm_prompt("Would you like to fine-tune the parameters?"):
+            finished = False
             while not finished:
-                selected = autocomplete_prompt("Select the parameter:", choices=all_configs.keys())
-                cur_val = all_configs[selected]
+                selected_param = autocomplete_prompt("Select the parameter:", choices=algo_configs.keys())
+                cur_val = algo_configs[selected_param]
                 vtype = type(cur_val)
 
                 while True:
-                    user_input = text_prompt(f"Set '{selected}' [{vtype.__name__}: {cur_val}]:")
+                    user_input = text_prompt(f"Set '{selected_param}' [{vtype.__name__}: {cur_val}]:")
                     try:
                         if user_input and user_input.lower() != "none":
-                            all_configs[selected] = vtype(user_input)
+                            algo_configs[selected_param] = vtype(user_input)
                         # Validate by setting params
-                        cls._AVAILABLE_ALGO[method]().set_params(**all_configs)
+                        algo_instance.set_params(**algo_configs)
                         break # Exit the loop if successfully updated and validated
-                    except ValueError as e:
-                        print(f"Invalid value for {selected}: {e}")
-                    except TypeError as e:
-                        print(f"Invalid type for {selected}: {e}")
+                    except (ValueError, TypeError) as e:
+                        print(f"Error updating {selected_param}: {e}")
 
-                configs[method][selected] = all_configs[selected]
+                configs[method][selected_param] = algo_configs[selected_param]
                 finished = not confirm_prompt("Continue adjusting parameters?")
         else:
             print("Skipping parameter tuning.")
